90a96c1dc4539e0bbd6f8d5701b600c4daed6d3f
Ubuntu <ubuntu@ip-172-31-31-233.us-west-2.compute.internal>
2013-07-11 19:00:53 +0000

    added index.html and modded web.js

--- web.js
+++ web.js
@@ -1,10 +1,17 @@
 var express = require('express');
 
+var fs = require('filesystem');
+
 var app = express.createServer(express.logger());
 
+var fil=fs.readFileSync('index.html')
+
 app.get('/', function(request, response) {
-  response.send('Hello World2');
-});
+  response.send(buf.toString(fil));
+})
+;
+
+//implement buffer 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
